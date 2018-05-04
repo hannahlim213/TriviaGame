2b5a3e41c9a7a1f192c318fb6e8d53fbea238c0f
Han Ban <hannah15198@gmail.com>
2018-05-04 10:26:34 -0700

    Hide and show depending on start, middle and endo of the game. Fixed timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,104 @@
-// Time interval functions
-function startTimer()
+$(document).ready(function () {
+    var correctArray = [];
+    var incorrectArray = [];
+    var amountCorrect = 0;
+    var amountIncorrect = 0;
+    var time = 10;
+    var answerValue;
+    var intervalId;
+    var displayIncorrect;
+var displayCorrect;
+    
+    
+    // Hide questions 
+    $(".questions").hide();
+    $(".scores").hide();
 
+    // on click start
+    // show div questions
+    $("#start").on("click", function () {
+        $(".questions").show();
+        // display initial value of timer
+        $("#time-left").html(time);
+        countDown()
+    });
 
-// Functions
-function start() {
-    $("#time-left").html("<p>")
-}
+    // onclick of finish 
+    // getAnswers()
+    // evaluate the answers. 
+    // For each index of answers array, add point to correct if index value is true. 
+    // If index value is false, then add to incorrect.
+    // empty div
+    $("#finish").on("click", function () {
+        //$(".questions").none();
+        // out of time 
+        // getAnswers()
+        getAnswers();
+        stop();
+        
+    });
+    
+    // User clicks radio buttons, store answers
+    $(".radioBtn").click(function (event) {
+        //var hold = $(this).val()
+        // console.log(hold)
+        // if (hold === "true") {
+        //     amountCorrect++;
+        // }
+        // else if (hold === "false") {
+        //     amountIncorrect++;
+    
+        //getAnswers();
+
+    })
+
+// push true to an correct array and false to incorrect array
+// lenght of correct and incorrect array will be the score
+    function getAnswers() {
+        for (i = 1; i < 5; i++) {
+            answerValue = $("input[name=q" + i + "]" + ":checked").val();
+           
+            if (answerValue === "true") {
+                correctArray.push(answerValue);
+                console.log(correctArray)
+            }
+            else if (answerValue === "false") {
+                incorrectArray.push(answerValue);
+                console.log (incorrectArray)
+            }
+            
+        }}
+
+   
+    function countDown() {
+       intervalId = setInterval(function () {
+            if (time === 0) {
+                $(".questions").hide();
+                stop()
+            } else {
+                time--;
+                $("#time-left").html(time)
+            }
+
+        }, 1000);
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+        displayScore();
+    }
+
+    function displayScore() {
+        $("#display").hide();
+        $(".questions").hide();
+        $(".scores").show();
+        
+        amountCorrect = correctArray.length;
+        amountIncorrect = incorrectArray.length;
+        displayCorrect = $("<div>").text("Correct: " + amountCorrect)
+        displayIncorrect =$("<div>").text("Incorrect: " + amountIncorrect);
+        $(".scores").html(displayCorrect, displayIncorrect);
+    }
+
+})
+
